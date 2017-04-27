841e74432f48be842e052f3886ca863507b47b34
GracieChristine <graciechristineli@gmail.com>
2017-04-27 10:39:17 -0600

    State & Logic: state changes.

--- src/component/App.js
+++ src/component/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Display from "./Display";
 import ButtonPanel from "./ButtonPanel";
+import calculate from "../logic/calculate";
 
 class App extends React.Component {
 
@@ -14,7 +15,7 @@ class App extends React.Component {
   }
 
   handleClick = (buttonName) => {
-    console.log("Clicking...", buttonName)
+    this.setState(calculate(this.state, buttonName));
   }
 
   render() {
